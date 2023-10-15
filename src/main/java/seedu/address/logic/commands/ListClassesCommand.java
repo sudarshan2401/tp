19d84ff8621b48815d2a76d8a2e72b42481866d2
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2023-10-15 09:15:42 +0800

    Add ListClasses command template

---
+++ src/main/java/seedu/address/logic/commands/ListClassesCommand.java
@@ -0,0 +1,14 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.model.Model;
+public class ListClassesCommand extends Command {
+    public static final String COMMAND_WORD = "list /c";
+    public static final String MESSAGE_SUCCESS = "Listed all classes";
+
+    @Override
+    public CommandResult execute(Model model) {
+
+    }
+}
