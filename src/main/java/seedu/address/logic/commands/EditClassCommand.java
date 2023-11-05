9fe00be6d176738dea2b5c118a4176ae26a61b3c
Kota Sudarshan Karanth <sudarshan2402@gmail.com>
2023-11-05 13:57:53 +0800

    Fix bug where UI was not updated
    Refresh UI upon update class info and fix bug where edit class command did not reflesh list before editing specified class

--- src/main/java/seedu/address/logic/commands/EditClassCommand.java
+++ src/main/java/seedu/address/logic/commands/EditClassCommand.java
@@ -61,6 +61,7 @@ public class EditClassCommand extends Command {
     @Override
     public CommandResult execute(Model model) throws CommandException {
         requireNonNull(model);
+        model.updateFilteredClassList(PREDICATE_SHOW_ALL_CLASSES);
         List<Class> lastShownList = model.getFilteredClassList();
 
         if (index.getZeroBased() >= lastShownList.size()) {
