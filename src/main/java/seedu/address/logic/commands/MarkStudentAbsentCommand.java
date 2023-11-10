38ad14953b397ba65175e9de2e01bee85c23f2bc
Donova Chan Jia Jun <donovanchanjiajun2001@gmail.com>
2023-11-10 18:58:12 +0800

    Remove unused import

--- src/main/java/seedu/address/logic/commands/MarkStudentAbsentCommand.java
+++ src/main/java/seedu/address/logic/commands/MarkStudentAbsentCommand.java
@@ -6,7 +6,6 @@ import static seedu.address.logic.parser.CliSyntax.PREFIX_STUDENT;
 
 import seedu.address.commons.core.index.Index;
 import seedu.address.commons.util.ToStringBuilder;
-import seedu.address.logic.Messages;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.module.Class;
